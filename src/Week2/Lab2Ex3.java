61e1631accbcee70873d3045d0323fcbc2c68f82
Janie <emerald1901@gmail.com>
2017-10-26 22:29:11 +0700

    Homework Thu 26/10

---
+++ src/Week2/Lab2Ex3.java
@@ -0,0 +1,18 @@
+// Write a program that prompts the user to enter an integer from 1 to 15
+// and displays a pyramid, as shown in the following sample run
+
+package Week2;
+
+import java.util.Scanner;
+
+public class Lab2Ex3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer from 1 to 15: ");
+        int number = input.nextInt();
+
+        if (number % 2 == 1) {
+            
+        }
+    }
+}
