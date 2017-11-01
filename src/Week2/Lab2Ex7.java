a7af5ab0c4679b0bfc993b4cd1ae91b61d176f8c
Janie <emerald1901@gmail.com>
2017-11-01 21:16:01 +0700

    HW 1/11

--- src/Week2/Lab2Ex7.java
+++ src/Week2/Lab2Ex7.java
@@ -13,24 +13,24 @@ public class Lab2Ex7 {
 
         System.out.print("Enter integers between 1 and 100: ");
         String[] strArr = input.nextLine().split(" ");
-        int[] numArr = new int[strArr.length];
-        for(int i = 0; i < strArr.length; i++) {
-            numArr[i] = parseInt(strArr[i]);
-        }
 
-        int currentNum = 0;
-        for(int i = 0; i < numArr.length - 1; i++) {
-            currentNum = numArr[i];
+        String currentNum;
+
+        for(int i = 0; i < strArr.length; i++) {
+            currentNum = strArr[i];
             int count = 1;
-            for(int j = i + 1; j < numArr.length; j++) {
-                if (numArr[j] == currentNum) {
+            for(int j = i + 1; j < strArr.length; j++) {
+                if (strArr[j].equals(currentNum)) {
                     count ++;
+                    strArr[j] = "";
                 }
             }
-            if (count == 1) {
-                System.out.println(currentNum + " occurs 1 time");
-            } else {
-                System.out.println(currentNum + " occurs " + count + " times");
+            if (strArr[i] != "") {
+                if (count == 1) {
+                    System.out.println(currentNum + " occurs 1 time");
+                } else {
+                    System.out.println(currentNum + " occurs " + count + " times");
+                }
             }
         }
 
